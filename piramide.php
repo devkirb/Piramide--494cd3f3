b053010001e9b58746718d35e86519bab68e9a3d
Maro Balestracci <2109711@talnet.nl>
2019-11-19 09:03:27 +0100

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hoe hoog wil je de piramide?" . PHP_EOL;
+$aantal = readline(); 
+if (is_numeric($aantal)){
+ for ($x = 1; $x <= $aantal; $x++) 
+ {
+	 for($x2 = 1; $x2 <= $x; $x2++ ) 
+    { 
+		echo "*";
+	}
+	echo PHP_EOL;
+ }
+}
